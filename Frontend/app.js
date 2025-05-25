b31be2add819b77985339daad74d21b0c8675361
techy-ops <krishnakeerthana401@gmail.com>
2025-05-26 00:21:30 +0530

    Create app.js

---
+++ Frontend/app.js
@@ -0,0 +1,13 @@
+@'
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from CodeWhiz!</h1>
+    </div>
+  );
+}
+
+export default App;
+'@ | Out-File -FilePath App.js -Encoding utf8
